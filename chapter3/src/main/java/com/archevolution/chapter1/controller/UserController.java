4e7d661a50e884658071034abfe35476a5d2d794
CodeDaShu <codedashu@126.com>
2020-01-30 09:06:47 +0800

    第三章节

--- chapter3/src/main/java/com/archevolution/chapter1/controller/UserController.java
+++ chapter3/src/main/java/com/archevolution/chapter1/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.archevolution.chapter1.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-	
+
 	@RequestMapping(value = "/queryAdmin")
     public String queryUser(){
 		System.out.println("queryAdmin");
