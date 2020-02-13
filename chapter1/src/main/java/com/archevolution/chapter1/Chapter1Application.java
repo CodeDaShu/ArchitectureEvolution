ae866ecb0e2c0fa08be031fe37d6e030dc5b711a
CodeDaShu <codedashu@126.com>
2020-02-13 10:53:18 +0800

    注解

--- chapter1/src/main/java/com/archevolution/chapter1/Chapter1Application.java
+++ chapter1/src/main/java/com/archevolution/chapter1/Chapter1Application.java
@@ -1,9 +1,17 @@
 package com.archevolution.chapter1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+//@ComponentScan
 @SpringBootApplication
+//@ImportResource("classpath:beans.xml")
 public class Chapter1Application {
 
 	public static void main(String[] args) {
