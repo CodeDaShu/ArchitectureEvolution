8e3185d90a6dbb4ab9deecb70a7ebd717600ec52
CodeDaShu <codedashu@126.com>
2020-02-22 22:36:10 +0800

    cache unless

--- chapter8/src/main/java/com/archevolution/chapter8/service/ProductCacheService.java
+++ chapter8/src/main/java/com/archevolution/chapter8/service/ProductCacheService.java
@@ -20,7 +20,8 @@ public class ProductCacheService {
 	ProductDao productDao;
 	
 	//@Cacheable("product")
-	@Cacheable(value="product", key="#productid", condition="#productid.length() <= 10") //productid 长度超过 10 的不会被缓存，举例使用，没有什么实际用处
+	//@Cacheable(value="product", key="#productid", condition="#productid.length() <= 10") //productid 长度超过 10 的不会被缓存，举例使用，没有什么实际用处
+	@Cacheable(value="product", key="#productid", unless="#result == null") //如果查询结果为空，就不缓存了
 	public Product findByProductid(String productid){
 		return productDao.findByProductid(productid);
 	}
