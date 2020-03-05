0b3c3fe96e40ff328d73c15834672daf6df0dba6
CodeDaShu <codedashu@126.com>
2020-03-05 13:31:16 +0800

    SetTemplateTest

--- chapter9/src/test/java/com/archevolution/chapter9/RedisTemplateTests.java
+++ chapter9/src/test/java/com/archevolution/chapter9/RedisTemplateTests.java
@@ -103,4 +103,57 @@ public class RedisTemplateTests {
     	//移除集合中右边的元素，放入另一个指定队列的最左边：B-E-A  F
     	redisTemplate.opsForList().rightPopAndLeftPush("list", "list2");
     }
+    
+    @Test
+    public void SetTemplateTest() {
+    	//添加元素
+    	redisTemplate.opsForSet().add("set1", "A");
+    	redisTemplate.opsForSet().add("set1", "B");
+    	redisTemplate.opsForSet().add("set1", "C");
+    	//添加重复元素，结果还是 A-B-C
+    	redisTemplate.opsForSet().add("set1", "A");
+    	
+    	redisTemplate.opsForSet().add("set2", "A", "B", "D");
+    	
+    	//取交集，结果是：A-B
+    	logger.info("set1 和 set2 的交集  = {}" , redisTemplate.opsForSet().intersect("set1", "set2"));
+    	//取交集，保存到 intersectset 中
+    	redisTemplate.opsForSet().intersectAndStore("set1", "set2", "intersectset");
+
+    	//取并集，结果是：A-B-C-D
+    	logger.info("set1 和 set2 的并集  = {}" , redisTemplate.opsForSet().union("set1", "set2"));
+
+    	//取差集，结果是：C
+    	logger.info("set1 和 set2 的差集  = {}" , redisTemplate.opsForSet().difference("set1", "set2"));
+    	
+    }
+    
+    @Test
+    public void ZSetTemplateTest() {
+    	redisTemplate.opsForZSet().add("zset", "A", 1);
+    	redisTemplate.opsForZSet().add("zset", "B", 1.2);
+    	redisTemplate.opsForZSet().add("zset", "C", 100);
+    	
+    	//B的分数 1.2 + 101.8 = 103
+    	redisTemplate.opsForZSet().incrementScore("zset", "B", 101.8);
+    	
+    	//返回某个元素排序后的位置（从小到大）
+    	logger.info("A 现在的排名（从小到大）  = {}" , redisTemplate.opsForZSet().rank("zset", "A"));
+    	
+    	//返回某个元素排序后的位置（从大到小）
+    	logger.info("A 现在的排名（从大到小）  = {}" , redisTemplate.opsForZSet().reverseRank("zset", "A"));
+
+    	redisTemplate.opsForZSet().add("zset", "D", 200);
+    	redisTemplate.opsForZSet().add("zset", "E", 300);
+    	
+    	//分数从大到小排序后，获取指定范围内所有元素（排名的顺序）；0  表示第一个，-1 表示所有
+    	logger.info("分数从大到小排序后，获取指定范围内所有元素（排名的顺序）  = {}" , redisTemplate.opsForZSet().reverseRange("zset", 1, 3));
+    	
+    	//分数从大到小排序后，获取分数在指定范围内的所有元素
+    	logger.info("分数从大到小排序后，获取分数在指定范围内的所有元素  = {}" , redisTemplate.opsForZSet().reverseRangeByScore("zset", 100, 200));
+    	
+    	//从大到小排序后，分数在 100-200 之间的，获取指定范围内所有元素（排名的顺序）
+    	logger.info("从大到小排序后，获取分数在 100-200 之间，获取指定范围内所有元素（排名的顺序）  = {}" , redisTemplate.opsForZSet().reverseRangeByScore("zset", 100, 200, 0, -1));
+    	
+    }
 }
